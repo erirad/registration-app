83d1e62accc450dfd3650858c33f1d0459e9adec
erirad <32630134+erirad@users.noreply.github.com>
2019-09-23 17:09:06 +0300

    Update config.php
    change domain

--- config.php
+++ config.php
@@ -3,4 +3,4 @@ define('DBHOST', 'localhost');
 define('DBUSER', 'root');
 define('DBPASS', 'root');
 define('DBNAME', 'try');
-define('DOMAIN', 'http://127.0.0.1:8001/');
+define('DOMAIN', 'https://mysterious-reef-00769.herokuapp.com/');
